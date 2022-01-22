3dfe71f43bf5712764e714bd65167bc3a97aea8a
Katya Sokolova <kototottoro@gmail.com>
2022-01-22 23:31:49 +0400

    upd
    Добавлено редактирование в компонент информации персонажа.

--- larp-web/src/app/modules/character/character.module.ts
+++ larp-web/src/app/modules/character/character.module.ts
@@ -10,10 +10,12 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogComponent } from './dialog/dialog.component';
+import {SplitButtonModule} from 'primeng/splitbutton';
+
 
 import { CharacterService } from 'src/app/shared/services/character.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DialogComponent } from './dialog/dialog.component';
 
 const appRoutes: Routes = [
   { path: '', component: CharacterComponent },
@@ -32,6 +34,7 @@ const appRoutes: Routes = [
     ToastModule,
     DialogModule,
     InputNumberModule,
+    SplitButtonModule
   ],
   declarations: [CharacterComponent, DialogComponent, ItemComponent],
   providers: [CharacterService, MessageService],
--- larp-web/src/app/modules/character/item/item.component.ts
+++ larp-web/src/app/modules/character/item/item.component.ts
@@ -3,6 +3,8 @@ import { CharacterService } from 'src/app/shared/services/character.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Character } from 'src/app/shared/models/character.model';
+import { MenuItem, MessageService } from 'primeng/api';
+import { ToastMessage } from 'src/app/shared/models/toast-message.model';
 
 @Component({
   selector: 'app-item',
@@ -10,21 +12,25 @@ import { Character } from 'src/app/shared/models/character.model';
   styleUrls: ['./item.component.less'],
 })
 export class ItemComponent implements OnInit {
+  displayDialog: boolean = false;
   public character!: Character;
   id!: string;
+  options: MenuItem[];
+
 
   constructor(
     private route: ActivatedRoute,
-    private characterService: CharacterService
+    private characterService: CharacterService,
+    private messageService: MessageService
   ) {
     //this.id = 'a896820b-af15-45ea-b0cd-88d18f7927a5';
+    
+    this.options = [];
   }
 
   ngOnInit() {
-    this.id = this.route.snapshot.params['id'];
-    this.characterService.getOne(this.id).subscribe((response: Character) => {
-      this.character = response;
-    });
+    this. id = this.route.snapshot.params['id'];
+    this.refresh();
   }
 
   getResourceBar(resource: Resource) {
@@ -38,5 +44,31 @@ export class ItemComponent implements OnInit {
     }
 
     return resourceBar;
+  }
+
+  edit() {
+    this.displayDialog = true;
+  }
+
+  closeDialog(toastMessage: ToastMessage) {
+    if (toastMessage) {
+      this.showMessage(toastMessage);
+    }
+    this.displayDialog = false;
+    this.refresh();
+  }
+
+  private refresh(){
+    this.characterService.getOne(this.id).subscribe((response: Character) => {
+      this.character = response;
+    });
+  }
+
+  private showMessage(toastMesssage: ToastMessage) {
+    this.messageService.add({
+      severity: toastMesssage.severity,
+      summary: toastMesssage.summary,
+      detail: toastMesssage.detail,
+    });
   }
 }
